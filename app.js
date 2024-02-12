b3a20907613fb934f2d05e84f4e3acb7cc11a782
adripower <adriantorres.gc@gmail.com>
2024-02-12 16:30:39 +0000

    modified:   app.js

--- app.js
+++ app.js
@@ -10,17 +10,11 @@ window.onload = function() {
         let what = ['my homework', 'the keys', 'the car'];
         let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
 
-        // estan con un let porque se quedaba indicando que no estan declaradas y marca con error pero se puede borra desde la linea 13 a la 18
 
-        let indice1 = who;
-        let indice2 = action;
-        let indice3 = what;
-        let indice4 = when;
-
-        indice1 = Math.floor(Math.random()*who.length);
-        indice2 = Math.floor(Math.random()*action.length);
-        indice3 = Math.floor(Math.random()*what.length);
-        indice4 = Math.floor(Math.random()*when.length);
+        let indice1 = Math.floor(Math.random()*who.length);
+        let indice2 = Math.floor(Math.random()*action.length);
+        let indice3 = Math.floor(Math.random()*what.length);
+        let indice4 = Math.floor(Math.random()*when.length);
 
        
 
